d99d9b4c514b1825d037ece56edfb663ca2f740f
Peter Meglis <petermeglis@gmail.com>
2020-05-14 14:41:10 -0400

    Add fields to task and update views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task
   include Mongoid::Document
   field :title, type: String
+  field :due_date, type: Date
+  field :due_time, type: Time
   field :completed, type: Mongoid::Boolean
+  field :completed_timestamp, type: DateTime
 end
