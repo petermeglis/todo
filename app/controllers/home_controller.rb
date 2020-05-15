cb39739144807176f77290ec2afb3e33cc316c0a
Peter Meglis <petermeglis@gmail.com>
2020-05-15 14:58:51 -0400

    Add upcoming page and refactor day partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,25 @@ class HomeController < ApplicationController
     @tasks = Task.where(completed: false)
   end
 
+  def today
+    @day_name = "Today " + Date.today.strftime("%A %b %d")
+    @tasks = Task.where(completed: false, due_date: Date.today)
+  end
+
+  def upcoming
+    @task_data = {}
+    7.times do |i|
+      date = Date.today + i.days
+
+      name = date.strftime("%A %b %d")
+      name = "Today " + name if i == 0
+      name = "Tomorrow " + name if i == 1
+
+      @task_data[name] = Task.where(completed: false, due_date: date)
+    end
+    @task_data
+  end
+
   def log
     @tasks = Task.where(completed: true)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get '/', to: 'home#home', as: 'home'
   post '/complete_task/:id', to: 'tasks#complete_task', as: 'complete_task'
   get '/log', to: 'home#log', as: 'log'
+  get '/today', to: 'home#today', as: 'today'
+  get '/upcoming', to: 'home#upcoming', as: 'upcoming'
   resources :tasks
   root 'home#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
